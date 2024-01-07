58be6b6090b931cdb669a2d2fa4dd8e44c15790f
talamus1972 <talamus1972@gmail.com>
2024-01-07 17:08:51 +0200

    2222

--- src/main.js
+++ src/main.js
@@ -6,40 +6,46 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector('.form');
 const imagesGallery = document.querySelector('.gallery');
-
-// const searchParamsDefaults = {
-//   key: '41564235-b9b3b0b401bd21d391a887255',
-//   q: 'black',
-//   image_type: 'photo',
-//   orientation: 'horizontal',
-//   safesearch: true,
-// };
+const loader = document.querySelector('.loader');
 
 const BASE_URL = "https://pixabay.com/api/";
 const API_KEY = "41564235-b9b3b0b401bd21d391a887255";
 
-const url = new URL(BASE_URL);
-url.searchParams.append("key", API_KEY);
+const searchParamsDefault = {
+  key: API_KEY,
+  q: '',
+  image_type: 'photo',
+  orientation: 'horizontal',
+  safesearch: true,
+};
+
+
+// const url = new URL(BASE_URL);
+// url.searchParams.append("key", API_KEY);
 // url.searchParams.append("q", "query");
-url.searchParams.append("image_type", "foto");
-url.searchParams.append("orientation", "horizontal");
-url.searchParams.append("safesearch", "true");
+// url.searchParams.append("image_type", "foto");
+// url.searchParams.append("orientation", "horizontal");
+// url.searchParams.append("safesearch", "true");
 
 const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
+  close: true,
+  enableKeyboard: true,
+  docClose: true,
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
-  const query = event.target.elements.search.value;
-   
-  renderImages(query);
-   event.currentTarget.reset();    
+  loader.style.display = 'block';
+searchParamsDefault.q = event.target.elements.search.value;
+const searchParams = new URLSearchParams(searchParamsDefault);
+renderImages(searchParams);
+event.currentTarget.reset();    
 });
 
-const getImages = (query) => {
-    return fetch(url + query)
+const getImages = (params) => {
+    return fetch(`https://pixabay.com/api/?${params}`)
         .then((response) => {
         if (!response.ok) {
         throw new Error("Somthing wrong")
@@ -65,34 +71,30 @@ const getHtml = ({hits}) => {
     lightbox.refresh();  
 }
 
-function renderImages(query) {
-    getImages(query)
+function renderImages(po) {
+getImages(po)
         .then(po => {
             getHtml(po);
         })
-        .catch(error => console.log(error))
+        .catch(po => {         
+        iziToast.error({
+        position: 'topRight',
+        messageColor: '#FFFFFF',
+        backgroundColor: '#EF4040',
+        messageSize: '16px',
+        messageLineHeight: '1.5',
+        message: 'Sorry, there are no images matching your search query. Please try again!',
+        });
+        })
+       .finally(() => {
+       loader.style.display = 'none';
+    });
  }
        
 
 
 
 
-                
-    // imagesGallery.innerHTML = query.reduce((html, image) => html + `
-    //     <li class="gallery-item">
-    //      <a href=${image.largeImageURL}> 
-    //       <img class="gallery-img" src =${image.webformatURL} alt=${image.tags}/>
-    //     </a>
-    //     <div class="gallery-text-box">
-    //       <p>Likes: <span class="text-value">${image.likes}</span></p>
-    //       <p>views: <span class="text-value">${image.views}</span></p>
-    //       <p>comments: <span class="text-value">${image.comments}</span></p>
-    //       <p>downloads: <span class="text-value">${image.downloads}</span></p>
-    //   </div>
-    //    </li>
-    // `, "")  
-    // lightbox.refresh();  
-  
          
 
 
