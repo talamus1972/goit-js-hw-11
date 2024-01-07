30d887a0bd44e51e44139ad8941e9c92acf2555c
talamus1972 <talamus1972@gmail.com>
2024-01-07 18:56:17 +0200

    22222222

--- src/main.js
+++ src/main.js
@@ -40,22 +40,20 @@ form.addEventListener('submit', event => {
   loader.style.display = 'block';
 searchParamsDefault.q = event.target.elements.search.value;
 const searchParams = new URLSearchParams(searchParamsDefault);
-renderImages(searchParams);
+getImages(searchParams);
 event.currentTarget.reset();    
 });
 
 const getImages = (params) => {
     return fetch(`https://pixabay.com/api/?${params}`)
         .then((response) => {
-        if (!response.ok) {
-        throw new Error("Somthing wrong")
-        }
-        return response.json();
-})    
-}
-
-const getHtml = ({hits}) => {
-     imagesGallery.innerHTML = hits.reduce((html, image) => html + `
+            if (!response.ok) {
+                throw new Error("Somthing wrong")
+            }
+            return response.json();
+        })
+        .then(({ hits }) => {
+            imagesGallery.innerHTML = hits.reduce((html, image) => html + `
         <li class="gallery-item">
          <a href=${image.largeImageURL}> 
           <img class="gallery-img" src =${image.webformatURL} alt=${image.tags}/>
@@ -67,29 +65,68 @@ const getHtml = ({hits}) => {
           <p>downloads: <span class="text-value">${image.downloads}</span></p>
       </div>
        </li>
-    `, "")  
-    lightbox.refresh();  
+    `, "")
+            lightbox.refresh();
+        })
+    
+        .catch(params => {
+            iziToast.error({
+                position: 'topRight',
+                message:
+                    'Sorry, there are no images matching your search query. Please try again!',
+            });
+        })
+        .finally(() => {
+            loader.style.display = 'none';
+        })
+    
 }
 
-function renderImages(po) {
-getImages(po)
-        .then(po => {
-            getHtml(po);
-        })
-        .catch(po => {         
-        iziToast.error({
-        position: 'topRight',
-        messageColor: '#FFFFFF',
-        backgroundColor: '#EF4040',
-        messageSize: '16px',
-        messageLineHeight: '1.5',
-        message: 'Sorry, there are no images matching your search query. Please try again!',
-        });
-        })
-       .finally(() => {
-       loader.style.display = 'none';
-    });
- }
+
+
+
+
+
+
+
+// const getHtml = ({hits}) => {
+//      imagesGallery.innerHTML = hits.reduce((html, image) => html + `
+//         <li class="gallery-item">
+//          <a href=${image.largeImageURL}> 
+//           <img class="gallery-img" src =${image.webformatURL} alt=${image.tags}/>
+//         </a>
+//         <div class="gallery-text-box">
+//           <p>Likes: <span class="text-value">${image.likes}</span></p>
+//           <p>views: <span class="text-value">${image.views}</span></p>
+//           <p>comments: <span class="text-value">${image.comments}</span></p>
+//           <p>downloads: <span class="text-value">${image.downloads}</span></p>
+//       </div>
+//        </li>
+//     `, "")  
+//     lightbox.refresh();  
+// }
+
+// function renderImages(params) {
+// getImages(params)
+//         .then(params => {
+//             getHtml(params);
+          
+//         })
+//     .catch(params => {      
+//               console.log("eeeeeeeeeeeee");
+        // iziToast.error({
+        // position: 'topRight',
+        // messageColor: '#FFFFFF',
+        // backgroundColor: '#EF4040',
+        // messageSize: '16px',
+        // messageLineHeight: '1.5',
+        // message: 'Sorry, there are no images matching your search query. Please try again!',
+        // });
+//         })
+//        .finally(() => {
+//        loader.style.display = 'none';
+//     });
+//  }
        
 
 
