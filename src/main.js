5f841fc594086ce8d4e65725bc55e322e2a8d7fb
talamus1972 <talamus1972@gmail.com>
2024-01-11 14:07:36 +0200

    error

--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
-
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 const form = document.querySelector('.form');
 const imagesGallery = document.querySelector('.gallery');
 const loader = document.querySelector('.loader');
 
-const BASE_URL = "https://pixabay.com/api/?";
-const API_KEY = "41564235-b9b3b0b401bd21d391a887255";
+const BASE_URL = 'https://pixabay.com/api/?';
+const API_KEY = '41564235-b9b3b0b401bd21d391a887255';
 
 const searchParamsDefault = {
   key: API_KEY,
@@ -27,35 +26,52 @@ const lightbox = new SimpleLightbox('.gallery a', {
   docClose: true,
 });
 
+const showLoader = state => {
+  loader.style.display = state ? 'block' : 'none';
+};
+
 form.addEventListener('submit', event => {
   event.preventDefault();
-  loader.style.display = 'block';
-searchParamsDefault.q = event.target.elements.search.value;
-const searchParams = new URLSearchParams(searchParamsDefault);
-getImages(searchParams);
-event.currentTarget.reset();    
+  showLoader(true);
+  searchParamsDefault.q = encodeURIComponent(
+    event.target.elements.search.value.trim()
+  );
+  if (searchParamsDefault.q === '') {
+    console.error('Please enter a valid search query.');
+    return;
+  }
+  const searchParams = new URLSearchParams(searchParamsDefault);
+  getImages(searchParams);
+  event.currentTarget.reset();
 });
 
-const getImages = (params) => {
-    return fetch(BASE_URL + `${params}`)
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(`Something went wrong. Please try again later.`)
-            } 
-            return response.json();
-        })
-       .then(({ hits }) => {
-            if (hits.length === 0) {
-                iziToast.error({
-                    position: 'topRight',
-                    messageColor: '#FFFFFF',
-                    backgroundColor: '#EF4040',
-                    titleSize: '8px',
-                    closeOnEscape: true,
-                    message: 'Sorry, there are no images matching your search query. Please try again!',
-                });
-            } else {
-                imagesGallery.innerHTML = hits.reduce((html, image) => html + `
+const getImages = params => {
+  showLoader(true);
+  return fetch(BASE_URL + `${params}`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Something went wrong. Please try again later.`);
+      }
+      return response.json();
+    })
+    .then(({ hits }) => {
+      imagesGallery.innerHTML = '';
+      if (hits.length === 0) {
+        iziToast.error({
+          position: 'topRight',
+          messageColor: '#FFFFFF',
+          backgroundColor: '#EF4040',
+          titleSize: '8px',
+          closeOnEscape: true,
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+        });
+      } else {
+        imagesGallery.innerHTML = '';
+        imagesGallery.innerHTML = hits.reduce(
+          (html, image) =>
+            html +
+            `
                     <li class="gallery-item">
                         <a href=${image.largeImageURL}> 
                             <img class="gallery-img" src=${image.webformatURL} alt=${image.tags}/>
@@ -67,36 +83,17 @@ const getImages = (params) => {
                             <p>Downloads: <span class="text-value">${image.downloads}</span></p>
                         </div>
                     </li>
-                `, "");
-                lightbox.refresh();
-            }
-        })
-    
-        .catch(error => {
-        console.error(error.message);
-        })
-        .finally(() => {
-            loader.style.display = 'none';
-        })
-    
-}
+                `,
+          ''
+        );
+        lightbox.refresh();
+      }
+    })
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    .catch(error => {
+      console.error(error.message);
+    })
+    .finally(() => {
+      showLoader(false);
+    });
+};
